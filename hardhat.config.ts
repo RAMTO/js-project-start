54053827b481a4c7006c84816591c60ab67b2660
Martin Dobrev <martindobrev87@gmail.com>
2023-12-21 13:43:32 +0200

    Config and script updated

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,34 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import 'dotenv/config';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    version: '0.8.19',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    // Arbitrum Sepolia Testnet
+    sepoliaArb: {
+      url: 'https://arbitrum-sepolia.blockpi.network/v1/rpc/public',
+      accounts: [process.env.WALLET_PRIVATE_KEY as string],
+    },
+    // Arbitrum Sepolia Testnet
+    sepoliaBase: {
+      url: 'https://base-sepolia.blockpi.network/v1/rpc/public',
+      accounts: [process.env.WALLET_PRIVATE_KEY as string],
+    },
+    // Linea Goerli Testnet
+    goerliLinea: {
+      url: 'https://linea-goerli.blockpi.network/v1/rpc/public',
+      accounts: [process.env.WALLET_PRIVATE_KEY as string],
+    },
+  },
 };
 
 export default config;
