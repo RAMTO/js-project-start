2af6f222496d720c0afe5bd4eda32d1eb4d8bb78
Martin Dobrev <martindobrev87@gmail.com>
2024-01-04 15:30:47 +0200

    More networks added

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -33,6 +33,11 @@ const config: HardhatUserConfig = {
       url: 'https://base-sepolia.blockpi.network/v1/rpc/public',
       accounts: [process.env.WALLET_PRIVATE_KEY as string],
     },
+    // Linea Mainnet
+    linea: {
+      url: 'https://1rpc.io/linea	',
+      accounts: [process.env.WALLET_PRIVATE_KEY as string],
+    },
     // Linea Goerli Testnet
     goerliLinea: {
       url: 'https://linea-goerli.blockpi.network/v1/rpc/public',
@@ -41,6 +46,31 @@ const config: HardhatUserConfig = {
     // Taiko Testnet
     taiko: {
       url: 'https://taiko-jolnir.blockpi.network/v1/rpc/public',
+      accounts: [process.env.WALLET_PRIVATE_KEY as string],
+    },
+    // Polygon zkEVM Mainnet
+    polygonZkEvm: {
+      url: 'https://polygon-zkevm.drpc.org',
+      accounts: [process.env.WALLET_PRIVATE_KEY as string],
+    },
+    // Scroll Mainnet
+    scroll: {
+      url: 'https://1rpc.io/scroll',
+      accounts: [process.env.WALLET_PRIVATE_KEY as string],
+    },
+    // Scroll Sepolia Testnet
+    sepoliaScroll: {
+      url: 'https://scroll-sepolia.blockpi.network/v1/rpc/public',
+      accounts: [process.env.WALLET_PRIVATE_KEY as string],
+    },
+    // zkSync Era Mainnet
+    zkSyncEra: {
+      url: 'https://zksync-era.blockpi.network/v1/rpc/public	',
+      accounts: [process.env.WALLET_PRIVATE_KEY as string],
+    },
+    // zkSync Sepolia Testnet
+    sepoliaZkSyncEra: {
+      url: 'https://sepolia.era.zksync.dev',
       accounts: [process.env.WALLET_PRIVATE_KEY as string],
     },
   },
